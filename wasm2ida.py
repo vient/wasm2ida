d13fd19cccad82a28fd5f245918029f8446733fe
vient <lozko.roma@gmail.com>
2022-02-05 21:24:40 +0300

    checkout and build WABT submodule

--- wasm2ida.py
+++ wasm2ida.py
@@ -1,10 +1,37 @@
 #!/usr/bin/env python3
+import os
+import sys
 import plumbum as pb
 from plumbum import cli
 
+DEV_NULL = open(os.devnull, "w")
+SELF_DIR = pb.local.path(__file__).dirname
+
+
+def get_wasm2c_cmd(quiet: bool = False):
+    output = sys.stdout if not quiet else DEV_NULL
+    wabt_dir: pb.Path = SELF_DIR / 'wabt'
+    build_dir: pb.Path = wabt_dir / 'build'
+
+    if not (wabt_dir / 'Makefile').exists():
+        if not quiet:
+            print('[*] checking out WABT submodule...', file=output)
+        with pb.local.cwd(SELF_DIR):
+            (pb.local['git']['submodule', 'update', '--init', '--recursive'] > output)()
+
+    if not build_dir.exists():
+        if not quiet:
+            print('[*] building wasm2c...', file=output)
+        build_dir.mkdir()
+        with pb.local.cwd(build_dir):
+            (pb.local['cmake']['..'] > output)()
+            (pb.local['cmake']['--build', '.', '-j32'] > output)()
+
+    return pb.local[build_dir / 'wasm2c']
+
 
 class Wasm2Ida(cli.Application):
-    verbose = cli.Flag(['v', 'verbose'], help='verbose output')
+    quiet = cli.Flag(['q', 'quiet'], help='supress all output')
 
     def main(self, wasm_path: cli.ExistingFile, result_path: str):
         result_path = pb.local.path(result_path)
@@ -13,6 +40,11 @@ class Wasm2Ida(cli.Application):
         result_dir = result_path.dirname
         if not result_dir.exists():
             raise ValueError(f'result directory {result_dir} does not exist')
+
+        wasm2c = get_wasm2c_cmd(self.quiet)
+
+        with pb.local.tempdir() as tmp:
+            pass
 
         raise NotImplementedError
 
