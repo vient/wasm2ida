1c7417e67a2b3265c98d6fa213df2a4c87b89501
vient <lozko.roma@gmail.com>
2022-05-22 04:38:41 +0300

    a lot of fixes

--- README.md
+++ README.md
@@ -13,11 +13,20 @@ git clone https://github.com/vient/wasm2ida
 ./wasm2ida/wasm2ida.py -h
 ```
 
-## TODO
+## Implemented features
 
-* Check that module does not import memory blocks
-* Merge memory sections in compile time to one array in `.wasm_data` section
-* Interpet memory offsets as raw pointers, `.wasm_data` is mapped to 0
-* Do something with `w2c_g0` register, all local variables need to be allocated on x86 stack
-* It would be nice if IDA somehow detected automatically that `wasm_rt_trap` itself is noreturn and not rely on `__builtin_trap`
-* Use C++ mangling instead of WebAssembly one so IDA will set correct function types automatically
+- [x] Mark all funcs as noinline
+- [x] Create stubs for imported funcs, link with them
+- [x] Check that module use only one memory block
+- [x] Check that module does not import memory blocks
+- [x] Merge memory sections in compile time to one array in `.wasm_data` section
+- [x] Interpet memory offsets as raw pointers, `.wasm_data` is mapped to 0
+- [x] Insert UD2 after `wasm_rt_trap`
+- [ ] ... it would be nice if IDA somehow detected automatically that `wasm_rt_trap` is noreturn and not rely on `__builtin_trap`
+- [ ] Use C++ mangling instead of WebAssembly one so IDA will set correct function types automatically
+- [ ] Do something with `w2c_g0` register, all local variables need to be allocated on x86 stack
+
+## TODO (research)
+* Mulpiple memory blocks
+* Extern memories
+* Extern tables
--- wasm2ida.py
+++ wasm2ida.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import contextlib
 import os
 import sys
 
@@ -21,9 +22,12 @@ def get_wasm2c_cmd(force_rebuild, output=sys.stdout):
 
     if force_rebuild or not wasm2c_path.exists():
         print('[*] building wasm2c...', file=output)
+        wabt_build_dir.mkdir()
+        with pb.local.cwd(wabt_build_dir):
+            (pb.local['cmake']['..'] > output)()
+            (pb.local['cmake']['--build', '.', '-j32'] > output)()
         with pb.local.cwd(WABT_DIR):
             (pb.local['make']['update-wasm2c'] > output)()  # just in case
-        wabt_build_dir.mkdir()
         with pb.local.cwd(wabt_build_dir):
             (pb.local['cmake']['..'] > output)()
             (pb.local['cmake']['--build', '.', '-j32'] > output)()
@@ -35,6 +39,12 @@ class Wasm2Ida(cli.Application):
     quiet = cli.Flag(['q', 'quiet'], help='supress all output')
     keep_wasm_checks = cli.Flag(['keep-wasm-checks'], help='to not remove checks like call stack depth')
     force_wabt_rebuild = cli.Flag(['force-wabt-rebuild'], help='rebuild WABT submodule even if wasm2c already exists')
+
+    build_dir = None
+
+    @cli.switch(['b', 'build-dir'], pb.LocalPath, help='build directory, default is temporary directory')
+    def _set_build_dir(self, build_dir):
+        self.build_dir = build_dir
 
     def main(self, wasm_path: cli.ExistingFile, result_path: pb.LocalPath):
         if result_path.is_dir():
@@ -48,27 +58,37 @@ class Wasm2Ida(cli.Application):
         gcc = pb.local['gcc']
         ld = pb.local['ld']
 
-        with pb.local.tempdir() as tmp:
-            data_c: pb.LocalPath = tmp / 'data.c'
-            imports_c: pb.LocalPath = tmp / 'data.imports.c'
-            data_o: pb.LocalPath = tmp / 'data.o'
-            imports_o: pb.LocalPath = tmp / 'imports.so'
+        with contextlib.ExitStack() as stack:
+            if self.build_dir is not None:
+                build_dir: pb.LocalPath = self.build_dir
+                build_dir.mkdir()
+                if not build_dir.exists():
+                    raise ValueError(f'failed to create build directory {build_dir}')
+                if not build_dir.is_dir():
+                    raise ValueError(f'build path {build_dir} is not a directory')
+            else:
+                build_dir = stack.enter_context(pb.local.tempdir())
+            data_c: pb.LocalPath = build_dir / 'data.c'
+            imports_c: pb.LocalPath = build_dir / 'data.imports.c'
+            data_o: pb.LocalPath = build_dir / 'data.o'
+            imports_o: pb.LocalPath = build_dir / 'imports.so'
 
-            (wasm2c[wasm_path, data_c] > output)()
+            (wasm2c[wasm_path, '-o', data_c] > output)()
             assert data_c.exists()
             assert imports_c.exists()
 
-            (WABT_DIR / 'wasm2c' / 'wasm-rt.h').copy(tmp)
-            (WABT_DIR / 'wasm2c' / 'wasm-rt-impl.c').copy(tmp)
-            (WABT_DIR / 'wasm2c' / 'wasm-rt-impl.h').copy(tmp)
+            (WABT_DIR / 'wasm2c' / 'wasm-rt.h').copy(build_dir)
+            (WABT_DIR / 'wasm2c' / 'wasm-rt-impl.c').copy(build_dir)
+            (WABT_DIR / 'wasm2c' / 'wasm-rt-impl.h').copy(build_dir)
 
             defines = []
             if self.keep_wasm_checks:
                 defines.append('-DWASM2IDA_KEEP_CHECKS')
 
-            (gcc['-m32', '-shared', imports_c, tmp / 'wasm-rt-impl.c', '-o', imports_o] > output)()
-            (gcc.__getitem__([*defines, '-m32', '-O2', '-fno-stack-protector', '-c', data_c, '-o', data_o]) > output)()
-            (ld['--no-dynamic-linker', '-T', (SELF_DIR / 'build' / 'script.ld'), data_o, '-L' + tmp,
+            (gcc['-m32', '-shared', imports_c, build_dir / 'wasm-rt-impl.c', '-o', imports_o] > output)()
+            (gcc.__getitem__([*defines, '-m32', '-O2', '-fno-stack-protector', '-fno-plt', '-fno-pic', '-c', data_c,
+                imports_o, '-o', data_o]) > output)()
+            (ld['--no-dynamic-linker', '-T', (SELF_DIR / 'build' / 'script.ld'), data_o, '-L' + build_dir,
                 '-l:' + imports_o.name, '-o', result_path] > output)()
 
 
